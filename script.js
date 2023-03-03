2f872c29b16c5dda1d39a9aff1b83a84f47e89bb
newsance88 <tlkokka@gmail.com>
2023-03-03 10:51:56 +0300

    пытаюсь настроить ввод данных

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-let form = document.querySelector('form');
-form.addEventListener('submit', function(event) {
-  event.preventDefault(); // предотвратить перезагрузку страницы
-  let name = document.querySelector('#name').value;
-  let age = document.querySelector('#age').value;
-  let city = document.querySelector('#city').value;
-  sendDataToServer(name, age, city); // отправить данные на сервер
-});
+Server.use(cors());
+
+
+async function getResponse ( ){
+  let response = await fetch ('newanimepage.html')
+  console.log(response)
+}
+
+getResponse()
